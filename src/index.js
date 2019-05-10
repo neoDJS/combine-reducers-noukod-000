8da88bd493150615862ffc62a696fd39067ad5f7
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 11:06:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import rootReducer from './reducers/rootReducer';
 
 
 // const store = createStore(manageAuthorsAndBooks, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+const store = createStore(rootReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 ReactDOM.render(
   <Provider store={store}>

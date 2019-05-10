22827a1aee603c1b917fc373bb9c4a1877c97da3
Dimanche Jhon Steeve <jsteeve@noukod.com>
2019-05-10 11:15:02 +0000

    Automatically backed up by Learn

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -16,7 +16,16 @@ function booksReducer(state = [], action) {
     case "REMOVE_BOOK":
       idx = state.indexOf(action.id);
       return [...state.slice(0, idx), ...state.slice(idx + 1)];
-
+      
+     case "ADD_BOOK":
+       let existingAuthor = state.filter(
+         author => author.authorName === action.book.authorName
+       );
+       if (existingAuthor.length > 0) {
+         return state;
+       } else {
+         return [...state, { authorName: action.book.authorName, id: uuid() }];
+       }
     default:
       return state;
   }
